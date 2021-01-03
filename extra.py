836fbfb36af0009055a3fca91720f430667bd62f
AWelch27 <awelch27@student.umuc.edu>
2021-01-02 20:13:22 -0600

    Color purple hexadecimal # added

--- extra.py
+++ extra.py
@@ -9,7 +9,7 @@ fig, ax = plt.subplots()
 data = {'UV': 8, 'Ragweed': 2, 'Grass': 2, 'Tree': 0, 'Mold': 4, 'Dust': 4}
 levels = list(data.values())
 attributes = list(data.keys())
-colors = ['#66ff66', '#ffff66', '#ff8c66', '#ff6666']
+colors = ['#66ff66', '#ffff66', '#ff8c66', '#ff6666', '#8A2BE2']
 
 ax.barh(attributes, levels, align = 'center', color = colors)
 ax.set_xlabel('Levels')
